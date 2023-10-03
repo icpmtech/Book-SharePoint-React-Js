6496c26ac44ea8b6f5feeb32b9fa9e9e5ccc149b
Pedro Martins <mourao.martins@gmail.com>
2023-10-03 10:55:53 +0100

    add

---
+++ code/src/webparts/creatingFirstProject/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ICreatingFirstProjectWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'CreatingFirstProjectWebPartStrings' {
+  const strings: ICreatingFirstProjectWebPartStrings;
+  export = strings;
+}
